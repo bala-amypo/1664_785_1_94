deac4818f8897e3e3a90755d09f2553c33df0d5b
bala-amypo <bala@amypo.in>
2025-12-21 08:44:39 +0000

    User commit

--- src/main/java/com/example/demo/Controller/OverFlowPredictionController.java
+++ src/main/java/com/example/demo/Controller/OverFlowPredictionController.java
@@ -3,7 +3,8 @@ package com.example.demo.controller;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.Tag;
-import org.springframew
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 
 
 

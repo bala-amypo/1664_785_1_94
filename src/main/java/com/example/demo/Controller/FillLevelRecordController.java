461e5d4b28117509c58739fc16dcb990a581f03a
bala-amypo <bala@amypo.in>
2025-12-21 08:38:39 +0000

    User commit

--- src/main/java/com/example/demo/Controller/FillLevelRecordController.java
+++ src/main/java/com/example/demo/Controller/FillLevelRecordController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
-import org.springframework.web.bind.annotation.
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.Get
 
 
 @RestController

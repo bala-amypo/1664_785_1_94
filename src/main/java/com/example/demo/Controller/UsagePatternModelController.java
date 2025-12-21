927dbe22b491094f9e6d45fe6096ae1c00fca57a
bala-amypo <bala@amypo.in>
2025-12-21 08:47:39 +0000

    User commit

--- src/main/java/com/example/demo/Controller/UsagePatternModelController.java
+++ src/main/java/com/example/demo/Controller/UsagePatternModelController.java
@@ -2,7 +2,10 @@ package com.example.demo.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.
+import org.springframework.web.bind.annotation.Tag;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 
 
 

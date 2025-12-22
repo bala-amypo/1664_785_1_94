b745730a48c823fbc735e86621f475db91a90192
bala-amypo <bala@amypo.in>
2025-12-22 08:47:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/BinServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BinServiceImpl.java
@@ -1,6 +1,5 @@
 package com.example.demo.service.impl;
 
-import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.model.Bin;
 import com.example.demo.repository.BinRepository;
 import com.example.demo.service.BinService;
@@ -17,42 +16,12 @@ public class BinServiceImpl implements BinService {
 
     @Override
     public Bin createBin(Bin bin) {
-        bin.setActive(true);  
+        bin.setActive(true);
         return binRepository.save(bin);
-    }
-
-    @Override
-    public Bin updateBin(Long id, Bin bin) {
-        Bin existingBin = binRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Bin not found with id: " + id));
-
-        existingBin.setName(bin.getName());
-        existingBin.setLocation(bin.getLocation());
-        existingBin.setCapacity(bin.getCapacity());
-
-        return binRepository.save(existingBin);
-    }
-
-    @Override
-    public Bin getBinById(Long id) {
-        return binRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Bin not found with id: " + id));
     }
 
     @Override
     public List<Bin> getAllBins() {
         return binRepository.findAll();
-    }
-
-    @Override
-    public void deactivateBin(Long id) {
-        Bin bin = binRepository.findById(id)
-                .orElseThrow(() ->
-                        new ResourceNotFoundException("Bin not found with id: " + id));
-
-        bin.setActive(false);
-        binRepository.save(bin);
     }
 }

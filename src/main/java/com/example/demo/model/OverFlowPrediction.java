815769904e66ec35123a3f79079d82844b3453b5
bala-amypo <bala@amypo.in>
2025-12-22 10:41:01 +0000

    User commit

--- src/main/java/com/example/demo/model/OverFlowPrediction.java
+++ src/main/java/com/example/demo/model/OverFlowPrediction.java
@@ -9,18 +9,20 @@ import java.time.LocalDateTime;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Table(name = "overflow_predictions")
 public class OverFlowPrediction {
-
-@Autowired
-private OverFlowPredictionRepository predictionRepository; // fix variable name
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
+    @JoinColumn(name = "bin_id", nullable = false)
     private Bin bin;
 
+    @Column(nullable = false)
     private Boolean predictedOverflow;
+
+    @Column(nullable = false)
     private LocalDateTime predictedAt;
 }

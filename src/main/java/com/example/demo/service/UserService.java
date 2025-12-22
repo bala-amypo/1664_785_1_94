9db00823915ed390fc5ba3559d044509109cb8a5
bala-amypo <bala@amypo.in>
2025-12-22 10:23:41 +0000

    User commit

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -3,10 +3,7 @@ package com.example.demo.service;
 import com.example.demo.model.User;
 
 public interface UserService {
-
-    User registerUser(String fullName, String email, String password);
-
-    User getByEmail(String email);
-
-    boolean exists(String email);
+    User registerUser(String name, String email, String password);
+    User getUserByEmail(String email);
+    boolean exists(String email); // this was missing in your impl
 }

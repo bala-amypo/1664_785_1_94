7cbad6936161c5e6b77a7fc3d0fbe8c595a01fd7
bala-amypo <bala@amypo.in>
2025-12-23 10:12:28 +0000

    User commit

--- src/main/java/com/example/demo/controller/ZoneController.java
+++ src/main/java/com/example/demo/controller/ZoneController.java
@@ -11,7 +11,6 @@ import java.util.List;
 @RequestMapping("/api/zones")
 @Tag(name = "Zones")
 public class ZoneController {
-
     private final ZoneService zoneService;
 
     public ZoneController(ZoneService zoneService) {

bb02fccda043325f75ad535b9642ceec0853ffb1
bala-amypo <bala@amypo.in>
2025-12-25 14:52:10 +0000

    User commit

--- src/main/java/com/example/demo/security/CustomerUserDetailsService.java
+++ src/main/java/com/example/demo/security/CustomerUserDetailsService.java
@@ -1,5 +1,33 @@
-import com.example.demo.security.CustomUserDetailsService;
+package com.example.demo.security;
 
-public class CustomUserDetailsService{
-    
+import com.example.demo.model.User;
+import com.example.demo.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomerUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomerUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByUsername(username)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("User not found: " + username));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getUsername())
+                .password(user.getPassword())
+                .roles(user.getRole())
+                .build();
+    }
 }

164085aefdade49c90ef27b0ff1e1015aa66b549
bala-amypo <bala@amypo.in>
2025-12-25 11:19:59 +0000

    User commit

--- src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
+++ src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
@@ -11,7 +11,9 @@ import java.util.Optional;
 @Repository
 public interface OverflowPredictionRepository extends JpaRepository<OverflowPrediction,Long>{
     OverflowPrediction save(OverflowPrediction prediction);
-    List<OverflowPrediction> findLatestPredictionsByZone(Long id);
+    @Query("SELECT o FROM OverflowPrediction o WHERE o.zone = :zone")
+List<OverflowPrediction> findLatestPredictionsByZone(@Param("zone") Long zone);
+
 
     List<OverflowPrediction> findByBinId(Long binId);
 

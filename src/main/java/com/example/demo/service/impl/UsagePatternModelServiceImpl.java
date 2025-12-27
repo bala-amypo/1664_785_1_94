6ad5bb4466663448624e43efd5eaefe42b97a6e4
bala-amypo <bala@amypo.in>
2025-12-27 10:13:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UsagePatternModelServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/UsagePatternModelServiceImpl.java
@@ -2,12 +2,12 @@ package com.example.demo.service.impl;
 
 import com.example.demo.model.UsagePatternModel;
 import org.springframework.stereotype.Service;
-
+import com.example.demo.service.UsagePatternModelService ;
 import java.util.HashMap;
 import java.util.Map;
 
 @Service
-public class UsagePatternModelServiceImpl {
+public class UsagePatternModelServiceImpl implements UsagePatternModelService {
 
     private final Map<Long, UsagePatternModel> usageMap = new HashMap<>();
 

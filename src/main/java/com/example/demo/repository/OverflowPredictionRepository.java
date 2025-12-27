b9a6347a557e28d4598900706af2ea29bb6c2d44
bala-amypo <bala@amypo.in>
2025-12-27 03:40:24 +0000

    User commit

--- src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
+++ src/main/java/com/example/demo/repository/OverflowPredictionRepository.java
@@ -1,22 +1,14 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.OverflowPrediction;
-import com.example.demo.model.Zone;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import java.util.List;
-import java.util.Optional;
 
-@Repository
-public interface OverflowPredictionRepository extends JpaRepository<OverflowPrediction,Long>{
-    OverflowPrediction save(OverflowPrediction prediction);
-   @Query("SELECT p FROM OverflowPrediction p WHERE p.bin.zone = :zone")
-List<OverflowPrediction> findLatestPredictionsForZone(@Param("zone") Zone zone);
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.OverflowPrediction;
 
-    List<OverflowPrediction> findByBinId(Long binId);
+public interface OverflowPredictionRepository
+        extends JpaRepository<OverflowPrediction, Long> {
 
+    List<OverflowPrediction>
+        findByZoneIdOrderByPredictionTimeDesc(Long zoneId);
 }
-
--- src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
+++ src/main/java/com/example/demo/repository/UsagePatternModelRepository.java
@@ -1,12 +1,14 @@
 package com.example.demo.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.demo.model.UsagePatternModel;
-import com.example.demo.model.Bin;
 import java.util.Optional;
 
-public interface UsagePatternModelRepository  extends JpaRepository<UsagePatternModel,Long>{
-Optional<UsagePatternModel> findTop1ByBinOrderByLastUpdatedDesc(Bin bin);
+import org.springframework.data.jpa.repository.JpaRepository;
 
+import com.example.demo.model.UsagePatternModel;
 
+public interface UsagePatternModelRepository 
+        extends JpaRepository<UsagePatternModel, Long> {
+
+    Optional<UsagePatternModel> 
+        findTopByBinIdOrderByLastUpdatedDesc(Long binId);
 }

4a375b9d6beb608d56ee3e1fcecd1280f63df4d5
bala-amypo <bala@amypo.in>
2025-12-27 16:45:45 +0000

    User commit

--- src/main/java/com/example/demo/model/FillLevelRecord.java
+++ src/main/java/com/example/demo/model/FillLevelRecord.java
@@ -1,41 +1,12 @@
 package com.example.demo.model;
 
-import javax.persistence.*;
+import lombok.Data;
 import java.time.LocalDateTime;
 
-@Entity
-@Table(name = "fill_level_records")
+@Data
 public class FillLevelRecord {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    
     private Double fillLevel;
     private LocalDateTime recordedAt;
-    
-    @ManyToOne
-    @JoinColumn(name = "bin_id")
     private Bin bin;
-    
-    // Constructors
-    public FillLevelRecord() {}
-    
-    public FillLevelRecord(Double fillLevel, LocalDateTime recordedAt, Bin bin) {
-        this.fillLevel = fillLevel;
-        this.recordedAt = recordedAt;
-        this.bin = bin;
-    }
-    
-    // Getters and Setters
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-    
-    public Double getFillLevel() { return fillLevel; }
-    public void setFillLevel(Double fillLevel) { this.fillLevel = fillLevel; }
-    
-    public LocalDateTime getRecordedAt() { return recordedAt; }
-    public void setRecordedAt(LocalDateTime recordedAt) { this.recordedAt = recordedAt; }
-    
-    public Bin getBin() { return bin; }
-    public void setBin(Bin bin) { this.bin = bin; }
 }

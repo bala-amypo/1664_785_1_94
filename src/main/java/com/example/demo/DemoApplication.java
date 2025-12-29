e8e869297988e70b8547cc6ff0f6d069326d163a
bala-amypo <bala@amypo.in>
2025-12-29 06:26:16 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,18 +2,25 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
-System.out.println(new BCryptPasswordEncoder().encode("madhavan2003"));
-		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-		if(encoder.matches("madhavan2003","")){
-			System.err.print("Password Matched");
-		}else{
-			System.out.print("Password Not Matched");
-		}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+
+        String rawPassword = "madhavan2003";
+        String encodedPassword = encoder.encode(rawPassword);
+
+        System.out.println(encodedPassword);
+
+        if (encoder.matches(rawPassword, encodedPassword)) {
+            System.out.println("Password Matched");
+        } else {
+            System.out.println("Password Not Matched");
+        }
+    }
 }
